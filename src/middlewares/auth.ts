82102a80eac95f1179966428e595696c1e401513
jack <juliocesar_19@hotmail.com.br>
2023-08-12 16:44:33 -0300

    projects import

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { User } from "../models/Projects";
+import { Projects } from "../models/Projects";
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 
